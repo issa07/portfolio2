aa1be2d3215ddb1650b2c66299382c93070d9475
Issa DEMIRCI <issa.demirci@epitech.eu>
2020-09-29 20:24:30 +0200

    portfolio2: navbar, bouton(back), profil_avatar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import Home from './components';
 
 function App() {
   return (
-    <div>
+    <>
+    <CssBaseline/>
       <Home />
-    </div>
+      
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
+
 import {
     AppBar,
     Toolbar,
     ListItem,
-    IconButton,
+    ListItemIcon,
     ListItemText,
+    IconButton,
     Avatar,
     Divider,
     List,
     Typography,
-    Box
+    Box, 
+    // Button,
 } from "@material-ui/core";
 
 import {
@@ -19,19 +24,136 @@ import {
     Apps,
     ContactMail
 } from "@material-ui/icons";
+// import { blue, green, red } from '@material-ui/core/colors';
+import avatar from "../media/avatar2.png"
+
+/********** CSS STYLES  ********/
+const useStyles = makeStyles(theme=>({
+    menuSliderContainer: {
+        width: 250,
+        height: "30rem",
+        background: "#511",
+    },
+    avatar: {
+        display: "block",
+        margin: "0.5rem auto",
+        width: theme.spacing(13),
+        height: theme.spacing(13)
+    },
+    listItem: {
+        color: "tan",
+    }
+}))
+/*******************************/
+
+const menuItems = [
+    {
+        listIcon: <Home />,
+        listText: "Home",
+    },
+    {
+        listIcon: <AssignmentInd />,
+        listText: "A propos de moi",
+    },
+    {
+        listIcon: <Apps />,
+        listText: "Projets",
+    },
+    {
+        listIcon: <ContactMail />,
+        listText: "Contacter",
+    }
+]
+// console.log(menuIcons);
 
 const Navbar = () => {
+
+    const [state, setState] = useState({
+        right: false
+    });
+
+    const toggleSlider = (slider, open) => {
+        setState({ ...state, [slider]: open});
+    };
+
+    const classes = useStyles() // Qui fait appel à la constante "useStyles" qui est déclaré tout en haut
+
+    const sideList = slider => (
+    <Box className={classes.menuSliderContainer} component="div">
+        <Avatar className={classes.avatar} src={avatar}  alt="Issa Demirci" />
+        <Divider />
+        <List>
+            {menuItems.map((lsItem, key) => (
+                <ListItem button key={key} >
+                    <ListItemIcon className={classes.listItem}> {lsItem.listIcon} </ListItemIcon>
+                    <ListItemText className={classes.listItem} primary={lsItem.listText} />
+                </ListItem>
+            ))}
+        </List>
+    </Box>
+    )
     return (
-        <Box >
+        <>
+        <Box className={classes.menuSliderContainer} component="div">
+            <Avatar className={classes.avatar} src={avatar}  alt="Issa Demirci" />
+            <Divider />
+            <List>
+                {menuItems.map((lsItem, key) => (
+                    <ListItem button key={key} >
+                        <ListItemIcon className={classes.listItem}> {lsItem.listIcon} </ListItemIcon>
+                        <ListItemText className={classes.listItem} primary={lsItem.listText} />
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+        
+        <Box>
             <AppBar position="static" style={{background: "#222" }}>
                 <Toolbar>
                     <IconButton>
-                        <ArrowBack />
+                        {/* <MuiThemeProvider theme={theme}> */}
+                            {/* <Button> Retour </Button> Qui remplace ArrowBack */}
+                            <ArrowBack style={{color: "tomato"}} />
+                        {/* </MuiThemeProvider> */}
                     </IconButton>
+                    <Typography variant="h5" style={{color: "tan"}} > Portfolio </Typography>
                 </Toolbar>
             </AppBar>
         </Box>
+        </>
     );
 };
+
+
+/*********************
+ *  Petit modification(entrainement) au niveau de bouton de "Back" 
+ * 
+ * 
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        background: red,
+    },
+    typography: {
+        fontSize: 15,
+    },
+    overrides: {
+        MuiButton: {
+            root: {
+                backgroundColor: "#E9967A",
+                '&:hover': {
+                    backgroundColor: "red",
+                }
+            },
+            
+        }
+    },
+}); 
+
+*************************************/
+
+
+
+
 
 export default Navbar;
