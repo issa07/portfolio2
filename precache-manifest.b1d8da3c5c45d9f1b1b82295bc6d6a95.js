9f42e9739e4bc1a82c159d464b191c3bfa57451c
Issa DEMIRCI <issa.demirci@epitech.eu>
2020-10-10 17:15:17 +0200

    Updates

---
+++ precache-manifest.b1d8da3c5c45d9f1b1b82295bc6d6a95.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cabef10a6c9b330b39456f0601d552f",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "6348c598d14652c44508",
+    "url": "/portfolio2/static/css/main.84dbd095.chunk.css"
+  },
+  {
+    "revision": "fe2165078046f3ffd041",
+    "url": "/portfolio2/static/js/2.7be6dfa9.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio2/static/js/2.7be6dfa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6348c598d14652c44508",
+    "url": "/portfolio2/static/js/main.1c503845.chunk.js"
+  },
+  {
+    "revision": "4b059261683b5be75625",
+    "url": "/portfolio2/static/js/runtime-main.1ee5a9c2.js"
+  },
+  {
+    "revision": "ecb80de0b89325e24ed9adbc653001f0",
+    "url": "/portfolio2/static/media/avatar3.ecb80de0.png"
+  },
+  {
+    "revision": "b517cb07797569ae2bdec23e32b8b5e5",
+    "url": "/portfolio2/static/media/coffee-apple2.b517cb07.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio2/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/portfolio2/static/media/html-css-javascript.2c7701aa.jpg"
+  }
+]);
